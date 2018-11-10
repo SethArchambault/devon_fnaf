679b5e7d4db0f789603dfc52e2bedf8854a114a2
Seth Archambault <seth@doercreator.com>
2018-11-09 22:06:14 -0500

    osx

--- share/project.c
+++ share/project.c
@@ -219,7 +219,6 @@ void game() {
                         fclose(f);
                     }
             }
-               float  fade_cos = fabs(cos(frame_long * 0.1f));
                static int cursor_color = 0;
                 static char * canvas = {0};
 #define CanvasW 16
@@ -442,7 +441,7 @@ void game() {
                 DrawRectangleLines(PixelFromCursorX(1), PixelFromCursorX(1), CanvasW*CursorPixelW, CanvasW*CursorPixelW, Fade(WHITE,0.3));
                 // :cursor
                 if (cursor.y > 0) {
-                    DrawRectangle(PixelFromCursorX(cursor.x), PixelFromCursorX(cursor.y), CursorPixelW, CursorPixelW, Fade(colors[cursor_color],fade_cos));
+                    DrawRectangle(PixelFromCursorX(cursor.x), PixelFromCursorX(cursor.y), CursorPixelW, CursorPixelW, Fade(colors[cursor_color],1));
                 }
                 DrawRectangleLines(PixelFromCursorX(cursor.x), PixelFromCursorX(cursor.y), CursorPixelW, CursorPixelW, GRAY);
             EndDrawing();
@@ -607,152 +606,55 @@ void game() {
 
             // @Hack: this should be later
             BeginDrawing();
-                ClearBackground(BLACK);
-                //ClearBackground(WHITE);
-                // :fade in
-                static float fade_cos;
-                //fade_cos = (1.0f + cos(frame_long * 0.05f)) / 3;
-                fade_cos = 1.0f;//(1.0f + cos(frame_long * 0.05f)) / 3;
-                    static Action player_action = STAND;
-                if (player_action == DEAD) {
-                    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(RED, fade_cos/3));
-                }
-                static int move_times = 0;
-                static int low_fire = 1;
-                if (fade_cos <= 0.0001) {
-                    low_fire = 1;
-                }
-                if (fade_cos >= 0.5 && low_fire == 1) {
-                    ++move_times; 
-                    low_fire = 0;
-                }
-                if (fade_cos <= 0.0001 && move_times >= 2) {
-                    move_times = 0;
-                    for (int i = 0; i <  monster_a->count; ++i) {
-                        Monster temp = monster_a->items[i];
-                        if (monster_a->items[i].x < player.x) temp.x++;
-                        if (monster_a->items[i].y < player.y) temp.y++;
-                        if (monster_a->items[i].x > player.x) temp.x--;
-                        if (monster_a->items[i].y > player.y) temp.y--;
-                        int collide_b = 0;
-                        for (int i2 = 0; i2 <  monster_a->count; ++i2) {
-                            if (i2 == i) continue;
-                            if (    player.x == temp.x && 
-                                    player.y == temp.y ) {
-                                collide_b = 1;
-                                break;
-                            }
-                            if (    monster_a->items[i2].x == temp.x && 
-                                    monster_a->items[i2].y == temp.y ) {
-                                collide_b = 1;
-                                break;
-                            }
-                        }
-                        if (collide_b == 0) { 
-                            monster_a->items[i] = temp;
-                        }
-                    } // for
-                } // if
-            static Vector2 mouse;
-            mouse = GetMousePosition();
-            //DrawCircleGradient(GetScreenWidth()/2+ Tile_Size /3, GetScreenHeight()/2, Tile_Size*2.5f, RED, BLACK);
-            if (player_action != DEAD && actionPressed()) {
-                flashlight_on = !flashlight_on;
-                //DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), RED);
-                {
-                     DrawCircleGradient(GetScreenWidth()/2+ Tile_Size /3, GetScreenHeight()/2, Tile_Size*.5f, RED, BLACK);
-                     //DrawCircle(GetScreenWidth()/2+ Tile_Size /3, GetScreenHeight()/2, Tile_Size*.5f, RED, BLACK);
-                    for (int i = 0; i <  monster_a->count; ++i) {
-                        if ( monster_a->items[i].x >= player.x_dest-1 && 
-                             monster_a->items[i].x <= player.x_dest+1 &&
-                             monster_a->items[i].y >= player.y_dest-1 && 
-                             monster_a->items[i].y <= player.y_dest+1 
-                           ) {
-                            --monster_a->count;
-                            monster_a->items[i] = monster_a->items[monster_a->count];
-
-                        }
-                    }
-                }
+            ClearBackground(BLACK);
+            // :fade in
+            static Action player_action = STAND;
+            if (   up()   && !player.moving_y) {
+                player.y_pixel_dest -= Tile_Size;
+                player.y_dest   -= 1;
+                player.moving_y = 1;
             }
-
-            /*
-            if (!player.moving_y && !player.moving_x && actionPressed()) {
-                // @Todo: 
-                monster_a->items[monster_a->count].x = player.x;
-                monster_a->items[monster_a->count].y = player.y;
-                monster_a->items[monster_a->count].color = GREEN;
-                ++monster_a->count;
-                assert(monster_a->count < Monster_Max);
-                // :write plants.data
-                    FILE *f = fopen("assets/plants.data", "w");
-                    assert(f != NULL);
-                    //print_monster_a("Before Write", monster_a);
-                    fwrite(monster_a, sizeof (Monster_a), 1, f);
-                    fclose(f);
+            if ( down()   && !player.moving_y) {
+                player.y_pixel_dest += Tile_Size;
+                player.y_dest += 1;
+                player.moving_y = 1;
             }
-            if (!player.moving_y && !player.moving_x && waterPressed()) {
-                water_a->items[water_a->count].x = player.x;
-                water_a->items[water_a->count].y = player.y;
-                ++water_a->count;
-                assert(water_a->count < Water_Max);
-                // :write plants.data
-                FILE *f = fopen("assets/water.data", "w");
-                assert(f != NULL);
-                //print_monster_a("Before Write", monster_a);
-                fwrite(water_a, sizeof (Water_a), 1, f);
-                fclose(f);
-            }
-            */
-
-            if (player_action != DEAD) {
-                if (   up()   && !player.moving_y) {
-                    player.y_pixel_dest -= Tile_Size;
-                    player.y_dest   -= 1;
-                    player.moving_y = 1;
-                }
-                if ( down()   && !player.moving_y) {
-                    player.y_pixel_dest += Tile_Size;
-                    player.y_dest += 1;
-                    player.moving_y = 1;
-                }
-                {
-                    int water_count = 0;
-                    for (int i = 0; i <  water_a->count; ++i) {
-                        if (water_a->items[i].x == player.x_dest && 
-                            water_a->items[i].y == player.y_dest ){
-                            ++water_count;
-                        }
-                    }
-                    if (water_count > 1) {
-                        player.y_dest = player.y;
-                        player.y_pixel_dest = player.y_pixel;
-                    }
-                }
-                if (right()   && !player.moving_x) {
-                    player.x_pixel_dest += Tile_Size;
-                    player.x_dest   += 1;
-                    player.moving_x = 1;
-                }
-                if ( left()   && !player.moving_x) {
-                    player.x_pixel_dest -= Tile_Size;
-                    player.x_dest   -= 1;
-                    player.moving_x = 1;
-                }
-                {
+            {
                 int water_count = 0;
-                    for (int i = 0; i <  water_a->count; ++i) {
-                        if (water_a->items[i].x == player.x_dest && 
-                            water_a->items[i].y == player.y_dest ){
-                            water_count++;
-                        }
-                    }
-                    if (water_count > 1) {
-                        player.x_dest = player.x;
-                        player.x_pixel_dest = player.x_pixel;
+                for (int i = 0; i <  water_a->count; ++i) {
+                    if (water_a->items[i].x == player.x_dest && 
+                        water_a->items[i].y == player.y_dest ){
+                        ++water_count;
                     }
                 }
-            } // alive
+                if (water_count > 1) {
+                    player.y_dest = player.y;
+                    player.y_pixel_dest = player.y_pixel;
+                }
+            }
+            if (right()   && !player.moving_x) {
+                player.x_pixel_dest += Tile_Size;
+                player.x_dest   += 1;
+                player.moving_x = 1;
+            }
+            if ( left()   && !player.moving_x) {
+                player.x_pixel_dest -= Tile_Size;
+                player.x_dest   -= 1;
+                player.moving_x = 1;
+            }
+            {
+            int water_count = 0;
+                for (int i = 0; i <  water_a->count; ++i) {
+                    if (water_a->items[i].x == player.x_dest && 
+                        water_a->items[i].y == player.y_dest ){
+                        water_count++;
+                    }
+                }
+                if (water_count > 1) {
+                    player.x_dest = player.x;
+                    player.x_pixel_dest = player.x_pixel;
+                }
+            }
             static float speed = 4.0f; 
             // :collision
             int collision = 0;
@@ -872,108 +774,15 @@ void game() {
                     player_rect.x = player.x_pixel;
                     player_rect.y = player.y_pixel - Tile_Size / 4;
                     if (player_action == STAND) {
-                        DrawTexturePro(person_stand_tex, (Rectangle) { 32 * frame, 0, 32, 32 }, player_rect, (Vector2){ 0, 0 }, 0, Fade(WHITE, fade_cos+.1));
+                        DrawTexturePro(person_stand_tex, (Rectangle) { 32 * frame, 0, 32, 32 }, player_rect, (Vector2){ 0, 0 }, 0, WHITE);
                     } 
                     else if (player_action == DEAD) {
-                        DrawTexturePro(person_walk_tex, (Rectangle) { 32 * frame, 0, 32, 32 }, player_rect, (Vector2){ -12, 64 }, 90, Fade(WHITE, fade_cos+.1));
+                        DrawTexturePro(person_walk_tex, (Rectangle) { 32 * frame, 0, 32, 32 }, player_rect, (Vector2){ -12, 64 }, 90, WHITE);
                     } else {
-                        DrawTexturePro(person_walk_tex, (Rectangle) { 32 * frame, 0, 32, 32 }, player_rect, (Vector2){ 0, 0 }, 0, Fade(WHITE, fade_cos+.1));
+                        DrawTexturePro(person_walk_tex, (Rectangle) { 32 * frame, 0, 32, 32 }, player_rect, (Vector2){ 0, 0 }, 0, WHITE);
                     }
                 EndMode2D();
                     // :flashlight
-
-                    if (player_action != DEAD && flashlight_on){
-                        float half = GetScreenWidth()/2;
-
-                        Vector2 midpoint;
-                        midpoint.x = GetScreenWidth()/2;
-                        midpoint.y = GetScreenHeight()/2 + 20 ;
-
-                        float adjacent = midpoint.x - mouse.x;
-                        float opposite = midpoint.y - mouse.y; 
-                        float alpha_radian = atan(opposite/ adjacent);  
-                        float beta_radian = alpha_radian - M_PI / 2;
-                        int cursor_y = 0;
-                        int line_height = 20;
-
-                        //static char * s = NULL;
-                        //if (!s) s = (char *)malloc(sizeof (char) * 32);
-                        //
-                        HideCursor();
-                        float radius = 80;
-
-                        Vector2 l_tan;
-                        l_tan.y = mouse.y - sin(beta_radian) * radius; // opposite
-                        l_tan.x = mouse.x - cos(beta_radian) * radius; // adjacent
-
-                        Vector2 r_tan;
-                        r_tan.y = mouse.y + sin(beta_radian) * radius; // opposite
-                        r_tan.x = mouse.x + cos(beta_radian) * radius; // adjacent
-
-                        Vector2 points[4];
-                        points[0] = midpoint;
-                        if (mouse.x > midpoint.x)  {
-                            points[1] = l_tan;
-                            points[2] = r_tan;
-                        }
-                        else {
-                            points[2] = l_tan;
-                            points[1] = r_tan;
-                        }
-                        //DrawPolyEx(points, 3, Fade(RED, fade_cos));
-                        DrawPolyEx(points, 3, Fade(RED,1.0));
-                        //DrawCircleGradient(mouse.x, mouse.y, radius, Fade(RED, fade_cos), Fade(BLACK, fade_cos));
-                        //DrawCircle(mouse.x, mouse.y, radius, Fade(RED, fade_cos));
-                        /*
-                            */
-
-                    }
-                // :noise
-#if false
-            {
-                static Image noise_img = {0};
-                static int noise_img_b = 0;
-                noise_img = GenImageWhiteNoise(GetScreenWidth(), GetScreenHeight(), 0.5);
-                static Texture2D noise_tex;
-                static int noise_tex_b = 0;
-                if (!noise_tex_b) {
-                    noise_tex_b = 1;
-                    noise_tex = LoadTextureFromImage(noise_img);
-                }
-                DrawTexture(noise_tex, 0, 0, Fade( RED, 0.5f));
-            }
-#endif
-#if 0
-            {
-                for (int i = 0; i <  monster_a->count; ++i) {
-                    if (monster_a->items[i].x == player.x_dest && 
-                        monster_a->items[i].y == player.y_dest ){
-                        player_action = DEAD;
-                            DrawText("Samari slice that like button!",
-                                    250, // xpos
-                                    80, // ypos
-                                    30, // fontsize
-                                    RED //textColor
-                                    );
-
-                    }
-                }
-            }
-#endif
-            static int time_hour = 1;
-                // brothers that play game at, for people to play games at!
-                // two brother that create games for other people 
-                // i got 'a
-
-                
-                /*
-                DrawText(sprintf(NULL, "%d AM", time_hour),
-                        GetScreenWidth()/2-100, // xpos
-                        100, // ypos
-                        60, // fontsize
-                        Fade(WHITE, 0.5-fade_cos) //textColor
-                        );
-                        */
             EndDrawing();
         }
     }//while
