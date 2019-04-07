0774042765f674fb04c4886d322b91cebb950e20
Seth Archambault <seth@doercreator.com>
2019-04-07 13:46:12 -0400

    Pizza Employee

---
+++ osx/README.md
@@ -0,0 +1,144 @@
+
+ideas
+
+ - eneimies can make thinks mobile
+ - build your own pizza ria simulation
+ - make the draw mode load pngs?
+
+- game_stuff image make them individual 
+ * notes: 
+ * - png, y - different direction x - animation / different states
+ * footstep sound
+ * https://www.youtube.com/watch?v=btiw_49DeUU
+ *  12am-6am 25 minutes long in real time
+ *  they move periodically, but they don't start moving till 3pm
+ *  you can walk around. 
+ *  close the security doors
+ *  light switches to scare them away
+ *  you turn on the generator to activate the camera and the doors, clock is powered by the generator
+ *  clock is like a real clock in the game
+ *  the doors close behind you and lock 
+ *  newspaper 
+ *  welcome to beaver bears pizzaria - home of the best beaver bear in town
+ *  some atmospheric sounds
+ *
+he looks mangled and dirty
+all of them
+
+- door logic
+- pathfinding
+- each door has a light
+- slower
+- time goes hour to hour
+- pops up on the hour, doesn't show minutes
+- stop showing the time 
+- 15 minutes for an hour
+- find stuff to get the door unlocked
+
+- make it so everything is darker until the lights are turned on
+
+main character is no longer an employee, they are a kid who wanders in
+
+enemies:
+- only 3 animatronics kill you, the rest just bring your fear gauge up
+- ones that kill you: Dark figures, giraffe, Benny 
+- Benny is the only one where your fear gauge goes up as you get near him
+
+first night
+Goal: get generator running
+- in one room there noose and a body, thats where you get the rope, and the keycard, to unlock the security room. The old security guard.
+- low dark ominous music that changes to high when the fear kicks in.
+- 12 - 1 (20 minutes)
+- clean the boys and girls bathrooms, map, bucket, cleaner, 5 toilets + 6 sinks (5 seconds to clean toilet, 10 seconds to clean sinks)
+- at 3 animatronics follow certain paths. (mark the map for the path, with arrows) 
+- room next to the kitchen is the kids room, with two shelves, 
+- music box you need to crank to keep the giraffe in the box
+- if the giaraffe gets out he comes right after you. 
+- music box makes jungle sounds safari desert sounds - loud nature noises, with rainfall
+- takes 20 seconds
+- you can hide in the security room
+- close the doors to escape
+- after a while you can go back to the music box and the giraffe goes back
+- wack an animatronic - minigame, star is on the head of the animatronic, Xs are on top of the heads, use the mouse. 
+- you have to try not to get killed
+- electrical wires for the generator
+- pliers for the generator
+- fuse for the fusebox right to the generator
+- generator is in the storage closet
+- wait for clock to hit 6am and then you win
+- his dad used to work here
+
+
+second night
+- hammer to fix the camera / doors things
+- toy plushy because you're a child (Scared meter, if it goes to low, there's nightmare animatronic, screen shakes,)
+	- the only way to get unscared is to hold your plushy
+- infinite pizza - fill the hunger meter (hunger halucinations, in the kitchen)
+- duct tape - install the cameras (in the kitchen)
+- find the cameras (storage with the generator)
+- install the cameras
+
+third night
+- text message "There are good animatronic spirits there to help you, but you've got to find them!"
+- find the vent that goes to the secret place but it's too small to fit through
+- but someone on the other side is giving you other things (good spirit animatronix)
+- they talk to you, "you're welcome"
+- pick up hammer
+- pick up crowbar (security room)
+- rope (security room)
+- pickaxe (room left of kitchen)
+- shovels (room left of kitchen
+- fireaxe 
+- building is on fire
+- fireaxes work on all doors but main door
+- crowbar use on the main door
+- pickaxe use on the animatronix (gets stuck)
+- shovel use on the animatronix 
+- you get out
+
+game ending:
+- your father has killed seven kids in a row
+- you go seem him at the prison.
+- you goto the prison that your father's in, and you bring the phone in.
+- you text the number, and your father's phone buzzes
+
+after demo ends:
+- black screen
+- website url pops up
+- www.horrorpixel.us
+- for full game click this link
+- must be clickable in the game
+
+epilogue:
+- father killed seven kids
+
+Business:
+- hire a professional artist
+- 
+
+Price Range:
+- 
+- $10 - $12 for the full game
+
+new character: purple guy
+- lures in kids at night
+- stuffs them in the animatronic suit
+
+plot 
+- little kid 10 years old, is by the water fishing
+- he sees the purple man telling him to come into the building
+- "free pizza!" "Okay"
+- goes inside, can't get any signal on the phone he finds (brand: iBannana), so he needs to get on the internet. 
+- benny has been missing 3-4 years (he's been turned into the beaver bear)
+- best friend benny has been missing since he was 6
+- he brings his phone he has to get online 
+- so you have to get on wifi
+- Phone is in the security room
+- "Whoever finds this phone, text me back - you're in danger."
+	- phone works, but maybe in the future it doesn't
+	- but there's no signal. Next up, find the wifi.
+	- The wifi password is over to the side 
+- once you get on wifi, you start to get text messages
+
+alllowercaseoneword
+ *  */
--- osx/project.c
+++ osx/project.c
@@ -1,153 +1,8 @@
 /*
- * [x] the ability to reload textures instantly
- * [x] the ability to reload monster locations
- * [x] benny the beaver bear
- * [x] move things that need to be live into state
- * [ ] display clock
- * [ ] reload triggers
- *  the ability to restart
-- game_stuff image make them individual 
- * notes: 
- * - png, y - different direction x - animation / different states
- * footstep sound
- * https://www.youtube.com/watch?v=btiw_49DeUU
- * ideas:
- *  - eneimies can make thinks mobile
- *  - build your own pizza ria simulation
- *  - make the draw mode load pngs?
- *  x -10 y -60
- *  12am-6am 25 minutes long in real time
- *  they move periodically, but they don't start moving till 3pm
- *  you can walk around. 
- *  close the security doors
- *  light switches to scare them away
- *  you turn on the generator to activate the camera and the doors, clock is powered by the generator
- *  clock is like a real clock in the game
- *  the doors close behind you and lock 
- *  newspaper 
- *  welcome to beaver bears pizzaria - home of the best beaver bear in town
- *  some atmospheric sounds
+ * Robustness
+ *   [ ] reload triggers
  *
-he looks mangled and dirty
-all of them
-
-- door logic
-- pathfinding
-- each door has a light
-- slower
-- time goes hour to hour
-- pops up on the hour, doesn't show minutes
-- stop showing the time 
-- 15 minutes for an hour
-- find stuff to get the door unlocked
-
-- make it so everything is darker until the lights are turned on
-
-main character is no longer an employee, they are a kid who wanders in
-
-enemies:
-- only 3 animatronics kill you, the rest just bring your fear gauge up
-- ones that kill you: Dark figures, giraffe, Benny 
-- Benny is the only one where your fear gauge goes up as you get near him
-
-first night
-Goal: get generator running
-- in one room there noose and a body, thats where you get the rope, and the keycard, to unlock the security room. The old security guard.
-- low dark ominous music that changes to high when the fear kicks in.
-- 12 - 1 (20 minutes)
-- clean the boys and girls bathrooms, map, bucket, cleaner, 5 toilets + 6 sinks (5 seconds to clean toilet, 10 seconds to clean sinks)
-- at 3 animatronics follow certain paths. (mark the map for the path, with arrows) 
-- room next to the kitchen is the kids room, with two shelves, 
-- music box you need to crank to keep the giraffe in the box
-- if the giaraffe gets out he comes right after you. 
-- music box makes jungle sounds safari desert sounds - loud nature noises, with rainfall
-- takes 20 seconds
-- you can hide in the security room
-- close the doors to escape
-- after a while you can go back to the music box and the giraffe goes back
-- wack an animatronic - minigame, star is on the head of the animatronic, Xs are on top of the heads, use the mouse. 
-- you have to try not to get killed
-- electrical wires for the generator
-- pliers for the generator
-- fuse for the fusebox right to the generator
-- generator is in the storage closet
-- wait for clock to hit 6am and then you win
-- his dad used to work here
-
-
-second night
-- hammer to fix the camera / doors things
-- toy plushy because you're a child (Scared meter, if it goes to low, there's nightmare animatronic, screen shakes,)
-	- the only way to get unscared is to hold your plushy
-- infinite pizza - fill the hunger meter (hunger halucinations, in the kitchen)
-- duct tape - install the cameras (in the kitchen)
-- find the cameras (storage with the generator)
-- install the cameras
-
-third night
-- text message "There are good animatronic spirits there to help you, but you've got to find them!"
-- find the vent that goes to the secret place but it's too small to fit through
-- but someone on the other side is giving you other things (good spirit animatronix)
-- they talk to you, "you're welcome"
-- pick up hammer
-- pick up crowbar (security room)
-- rope (security room)
-- pickaxe (room left of kitchen)
-- shovels (room left of kitchen
-- fireaxe 
-- building is on fire
-- fireaxes work on all doors but main door
-- crowbar use on the main door
-- pickaxe use on the animatronix (gets stuck)
-- shovel use on the animatronix 
-- you get out
-
-game ending:
-- your father has killed seven kids in a row
-- you go seem him at the prison.
-- you goto the prison that your father's in, and you bring the phone in.
-- you text the number, and your father's phone buzzes
-
-after demo ends:
-- black screen
-- website url pops up
-- www.horrorpixel.us
-- for full game click this link
-- must be clickable in the game
-
-epilogue:
-- father killed seven kids
-
-Business:
-- hire a professional artist
-- 
-
-Price Range:
-- 
-- $10 - $12 for the full game
-
-new character: purple guy
-- lures in kids at night
-- stuffs them in the animatronic suit
-
-plot 
-- little kid 10 years old, is by the water fishing
-- he sees the purple man telling him to come into the building
-- "free pizza!" "Okay"
-- goes inside, can't get any signal on the phone he finds (brand: iBannana), so he needs to get on the internet. 
-- benny has been missing 3-4 years (he's been turned into the beaver bear)
-- best friend benny has been missing since he was 6
-- he brings his phone he has to get online 
-- so you have to get on wifi
-- Phone is in the security room
-- "Whoever finds this phone, text me back - you're in danger."
-	- phone works, but maybe in the future it doesn't
-	- but there's no signal. Next up, find the wifi.
-	- The wifi password is over to the side 
-- once you get on wifi, you start to get text messages
-
-alllowercaseoneword
- *  */
+ */
 
 #include <math.h>
 #include <string.h>
@@ -507,10 +362,8 @@ void game() {
                 for (int i = 0; i < Dialogue_Max; ++i) {
                     (*dialogue_queue)[i][0] = '\0';
                 }
-                //AddDialogue(dialogue_queue, "I wish I could do some fishing...");
-                AddDialogue(dialogue_queue, "It looks like it's going to rain..");
-                //AddDialogue(dialogue_queue, "Too bad I have to get to my new job...");
-                AddDialogue(dialogue_queue, "I better get home..");
+                AddDialogue(dialogue_queue, "I wish I could do some fishing...");
+                AddDialogue(dialogue_queue, "Too bad I have to get to my new job...");
                 changeState(&state, DialogueState);
             }
         }
@@ -774,8 +627,9 @@ void game() {
 
             if (!triggers[ViewsDarkness] && player->y == -41 && player->x == -5) {
                 triggers[ViewsDarkness] = 1;
-                AddDialogue(dialogue_queue, "Weird - the way home is covered in darkness..");
-                AddDialogue(dialogue_queue, "..Maybe I can find a local business, and call home.");
+                AddDialogue(dialogue_queue, "Sometimes I wonder if there is a world beyond the darkness..");
+                AddDialogue(dialogue_queue, "But try as I might, I can't bring myself to step foot into it.");
+                AddDialogue(dialogue_queue, "As if there is an invisible force compelling me...");
                 changeState(&state, DialogueState);
             }
 
@@ -797,9 +651,8 @@ void game() {
                 objects->items[14].actionFrame = 0;
                 hide_outdoors = 1;
 
-                AddDialogue(dialogue_queue, "Oh no! The wind slammed the door on me!");
-                AddDialogue(dialogue_queue, "Hello? Is anyone here?");
-                //AddDialogue(dialogue_queue, "And it won't budge! Guess I'm here till the next shift comes for me..");
+                AddDialogue(dialogue_queue, "Dang! The wind slammed the door on me!");
+                AddDialogue(dialogue_queue, "And it won't budge! Guess I'm here till the next shift comes for me..");
                 
                 changeState(&state, DialogueState);
             }
@@ -1131,6 +984,7 @@ void game() {
                 }
             }
             if (should_jump_scare) {
+                state.clock_display = 0;
                 state.normal_control = 0;
                 state.normal_display = 0;
                 clock[JumpscareClock]++;
@@ -1166,10 +1020,12 @@ void game() {
             print("You were Eaten", &print_data);
             print_data.fontsize = 46;
             print("Now you'll never solve the mystery.", &print_data);
+            /*
             if (clock[FailureClock] < 80) {
                 print("Type R to Restart", &print_data);
                 state.failure_control = 1;
             }
+            */
         }
 
         // :clock display
@@ -1189,7 +1045,7 @@ void game() {
             int military_hours = (total_hours % 24) ;
             int hours =  ((military_hours + 11) % 12) + 1;
             int minutes = total_minutes % 60;
-            total_seconds += 1;
+            total_seconds += 7;
 
             char am_pm[4] = "AM";
             if (military_hours >= 12) {
@@ -1237,7 +1093,7 @@ void game() {
         } // display debug
 
 		// :darkness
-            DrawRectangle(0, 0, screen.x, screen.y, Fade(BLACK, 0.7));
+            //DrawRectangle(0, 0, screen.x, screen.y, Fade(BLACK, 0.7));
 
         EndDrawing();
     }//while
